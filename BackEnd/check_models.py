79ac467358df5c80b53ab30a498decb557294c92
Daemon3T <tokyoken24@gmail.com>
2026-01-14 23:35:18 +0800

    .env and changing the file name from container file to dockerfile

--- BackEnd/check_models.py
+++ BackEnd/check_models.py
@@ -1,16 +1,14 @@
 from google import genai
 import os
 
-# Your actual key
-API_KEY = "AIzaSyBXu9GikPQ1X46N3ZNgie88WAoyX4TBlm8" 
+API_KEY = os.getenv("GOOGLE_API_KEY")
 
 client = genai.Client(api_key=API_KEY)
 
 print("--- FETCHING YOUR AVAILABLE MODELS ---")
 try:
-    # List models (v1beta is the default for this library)
     for model in client.models.list():
         name = model.name.split("/")[-1]
-        print(f" Found: {name}")
+        print(f"Found: {name}")
 except Exception as e:
-    print(f" Error: {e}")
+    print(f"Error: {e}")
