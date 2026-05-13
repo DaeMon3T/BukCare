cb5bf5c27806c587cd9f4113e17e6e1e15a6af4a
Daemon3T <tokyoken24@gmail.com>
2026-05-14 07:05:11 +0800

    Api Calling fix

--- FrontEnd/src/services/BaseAPI.ts
+++ FrontEnd/src/services/BaseAPI.ts
@@ -1,7 +1,7 @@
 // src/services/BaseAPI.ts
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 if (!API_BASE_URL) {
   throw new Error("VITE_API_BASE_URL is not defined");
